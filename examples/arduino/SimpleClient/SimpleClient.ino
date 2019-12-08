244466a6f3599f384d8b5fa5cf763e56895eb854
fredilarsen <fredilarsen@hotmail.com>
2019-12-08 18:50:07 +0100

    Fixed  arduino example, tested on Uno

--- examples/arduino/SimpleClient/SimpleClient.ino
+++ examples/arduino/SimpleClient/SimpleClient.ino
@@ -1,16 +1,22 @@
 #define PJON_INCLUDE_ETCP
 #include <ReconnectingMqttClient.h>
 
+// Ethernet configuration for this device, MAC must be unique!
+byte mac[] = {0xDE, 0x11, 0x4E, 0x1F, 0xFE, 0xE1};
+
 void receive_callback(const char *topic, const uint8_t *payload, uint16_t len, void *) {
   Serial.print("Received topic: "); Serial.print(topic); 
   Serial.print(", message: "); Serial.write(payload, len); 
   Serial.println();
 }
 
-uint8_t ip[] = { 127,0,0,1 };
+uint8_t ip[] = { 192,168,1,71 };
 ReconnectingMqttClient client(ip, 1883, "reconnectingclient");
 
 void setup() {
+  Serial.begin(115200);
+  while (Ethernet.begin(mac) == 0) delay(5000); // Wait for DHCP response
+
   // Test publishing of a retained message
   client.set_receive_callback(receive_callback, NULL);
   bool ok = client.publish("hellotopic", (uint8_t*) "hihello", 7, true, 1);
